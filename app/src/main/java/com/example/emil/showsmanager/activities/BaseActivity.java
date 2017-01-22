3a01337fd26a2fc2cc3fc9af773e22e0513b07b8
Emil <mazikem@gmail.com>
2017-01-23 00:49:46 +0100

    Base activity naming update

--- app/src/main/java/com/example/emil/showsmanager/activities/BaseActivity.java
+++ app/src/main/java/com/example/emil/showsmanager/activities/BaseActivity.java
@@ -5,12 +5,10 @@ import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.WindowManager;
 import android.widget.FrameLayout;
 import com.example.emil.showsmanager.R;
 
@@ -22,7 +20,7 @@ public class BaseActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_base);
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             getWindow().setStatusBarColor(getResources().getColor(R.color.colorPrimaryDark));
