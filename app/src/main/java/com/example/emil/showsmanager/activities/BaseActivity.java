74a46abb6f1f8b90dd4358d4f4078eb4a94623ec
Emil <mazikem@gmail.com>
2017-01-23 00:37:18 +0100

    renaming BaseActivity

--- app/src/main/java/com/example/emil/showsmanager/activities/BaseActivity.java
+++ app/src/main/java/com/example/emil/showsmanager/activities/BaseActivity.java
@@ -1,11 +1,16 @@
 package com.example.emil.showsmanager.activities;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
+import android.view.WindowManager;
 import android.widget.FrameLayout;
 import com.example.emil.showsmanager.R;
 
@@ -18,6 +23,15 @@ public class BaseActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().setStatusBarColor(getResources().getColor(R.color.colorPrimaryDark));
+        }
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
 
         frameLayout = (FrameLayout) findViewById(R.id.content_frame);
         bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
@@ -41,6 +55,27 @@ public class BaseActivity extends AppCompatActivity {
                         return true;
                     }
                 });
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                // User chose the "Settings" item, show the app settings UI...
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+
+        }
     }
 }
 
