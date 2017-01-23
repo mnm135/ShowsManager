0ae22eb4bd873ac6a717f2514765fd632576cc02
Emil <mazikem@gmail.com>
2017-01-23 01:01:55 +0100

    more project cleaning

--- app/src/main/java/com/example/emil/showsmanager/activities/SearchResultDetailsActivity.java
+++ app/src/main/java/com/example/emil/showsmanager/activities/SearchResultDetailsActivity.java
@@ -15,23 +15,21 @@ import com.example.emil.showsmanager.R;
 public class SearchResultDetailsActivity extends BaseActivity {
     String showId;
 
-   @Override
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-       LayoutInflater inflater = (LayoutInflater) this
-               .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-       View contentView = inflater.inflate(R.layout.activity_search_result_details, null, false);
-       frameLayout.addView(contentView, 0);
+        LayoutInflater inflater = (LayoutInflater) this
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View contentView = inflater.inflate(R.layout.activity_search_result_details, null, false);
+        frameLayout.addView(contentView, 0);
 
 
-
-
-       if (savedInstanceState == null) {
-           getSupportFragmentManager().beginTransaction()
-                   .add(R.id.container, new SearchResultMoreDetails())
-                   .commit();
-       }
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new SearchResultMoreDetails())
+                    .commit();
+        }
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -41,23 +39,4 @@ public class SearchResultDetailsActivity extends BaseActivity {
     }
 
 
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_search_result_details, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-    public String getShowId() {
-        return showId;
-    }
 }
