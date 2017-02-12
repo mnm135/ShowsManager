9a355a61c379b5b9b05845c66e6d4c7ee305197d
Emil <mazikem@gmail.com>
2017-02-13 00:06:52 +0100

    Fixed days counting bug.

--- app/src/main/java/com/example/emil/showsmanager/models/SubscribedShow.java
+++ app/src/main/java/com/example/emil/showsmanager/models/SubscribedShow.java
@@ -2,16 +2,11 @@ package com.example.emil.showsmanager.models;
 
 import com.google.firebase.database.IgnoreExtraProperties;
 
-import org.joda.time.Days;
+
 import org.joda.time.LocalDate;
+import org.joda.time.Period;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.util.Date;
-
-/**
- * Created by Emil on 21.12.2016.
- */
 
 @IgnoreExtraProperties
 public class SubscribedShow {
@@ -28,32 +23,25 @@ public class SubscribedShow {
     public String nextEpSeason;
 
     public SubscribedShow() {
-
     }
-//@TODO
+
     public SubscribedShow(String id, String name, String nextEpisodeAirdate, String imageUrl, String status) {
         this.id = id;
         this.name = name;
         this.nextEpisodeAirdate = nextEpisodeAirdate;
         this.imageUrl = imageUrl;
         this.status = status;
-        //this.timeToNextEpisode = calculateTimeToNextEpisode(nextEpisodeAirdate);
     }
 
     public SubscribedShow(String id, String name, String nextEpisodeAirdate, String imageUrl,
                           String status, String airtime, String channel, String nextEpNumber,
                           String nextEpSeason) {
-        this.id = id;
-        this.name = name;
-        this.nextEpisodeAirdate = nextEpisodeAirdate;
-        this.imageUrl = imageUrl;
-        this.status = status;
+        this(id, name, nextEpisodeAirdate, imageUrl, status);
         this.airtime = airtime;
         this.channel = channel;
         this.nextEpNumber = nextEpNumber;
         this.nextEpSeason = nextEpSeason;
         this.timeToNextEpisode = calculateTimeToNextEpisode(nextEpisodeAirdate);
-
     }
 
     public void setId(String id) {
@@ -76,28 +64,19 @@ public class SubscribedShow {
     public String getNextEpisodeAirdate() {
         return nextEpisodeAirdate;
     }
+
     public void setImageUrl(String imageUrl) {
         this.imageUrl = imageUrl;
     }
+
     public String getImageUrl() {
         return imageUrl;
     }
 
-
-
-    /*
-
-    public String timeToNextEpisode;
-    public String status;
-    public String airtime;
-    public String channel;
-    public String nextEpNumber;
-    public String nextEpSeason;
-     */
-
     public void setTimeToNextEpisode(String timeToNextEpisode) {
         this.timeToNextEpisode = timeToNextEpisode;
     }
+
     public String getTimeToNextEpisode() {
         return timeToNextEpisode;
     }
@@ -105,12 +84,15 @@ public class SubscribedShow {
     public void setStatus(String status) {
         this.status = status;
     }
+
     public String getStatus() {
         return status;
     }
+
     public void setAirtime(String airtime) {
         this.airtime = airtime;
     }
+
     public String getAirtime() {
         return airtime;
     }
@@ -118,6 +100,7 @@ public class SubscribedShow {
     public void setChannel(String channel) {
         this.channel = channel;
     }
+
     public String getChannel() {
         return channel;
     }
@@ -125,18 +108,18 @@ public class SubscribedShow {
     public void setNextEpNumber(String nextEpNumber) {
         this.nextEpNumber = nextEpNumber;
     }
+
     public String getNextEpNumber() {
         return nextEpNumber;
     }
 
-
     public void setNextEpSeason(String nextEpSeason) {
         this.nextEpSeason = nextEpSeason;
     }
+
     public String getNextEpSeason() {
         return nextEpSeason;
     }
-
 
     public void setTimeToNextEpisode() {
         this.timeToNextEpisode = calculateTimeToNextEpisode(this.nextEpisodeAirdate);
@@ -146,26 +129,12 @@ public class SubscribedShow {
         return timeToNextEpisode;
     }
 
-
-    //@TODO resources
-    public String calculateTimeToNextEpisode(String airdate) {
-        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+    //@TODO resources + timezone support
+    private String calculateTimeToNextEpisode(String airdate) {
         Date date = new Date();
-        String today = dateFormat.format(date);
+        LocalDate today = new LocalDate(date);
+        Period period = new Period(today, new LocalDate(airdate));
 
-        //String remainingTime =
-
-        int days = Days.daysBetween(new LocalDate(date),
-                new LocalDate(airdate)).getDays();
-
-        if (days == 0) {
-            return "today!";
-        }
-        return String.valueOf(days);
-
-
+        return String.valueOf(period.getDays());
     }
-
-
-
 }
