d8eced4eaa7c1c80ae4fd91f0af7c6020d9a848d
Emil <mazikem@gmail.com>
2017-02-21 20:23:06 +0100

    Added show name to grid of subscribed shows.

--- app/src/main/java/com/example/emil/showsmanager/adapters/SubscribedShowsGridAdapter.java
+++ app/src/main/java/com/example/emil/showsmanager/adapters/SubscribedShowsGridAdapter.java
@@ -1,11 +1,14 @@
 package com.example.emil.showsmanager.adapters;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.example.emil.showsmanager.R;
 import com.example.emil.showsmanager.models.firebase.FirebaseShow;
 import com.squareup.picasso.Picasso;
 
@@ -34,16 +37,33 @@ public class SubscribedShowsGridAdapter extends BaseAdapter {
 
     public View getView(int position, View convertView, ViewGroup parent) {
         ImageView imageView;
+        View gridView;
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+
         if (convertView == null) {
-            imageView = new ImageView(context);
-            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
-            imageView.setPadding(8, 8, 8, 8);
+            gridView = new View(context);
+            gridView = inflater.inflate(R.layout.single_grid_item_subscribed_shows, null);
+            TextView nameTextView = (TextView) gridView.findViewById(R.id.subscribed_grid_show_name);
+            ImageView posterImageView = (ImageView) gridView.findViewById(R.id.subscribed_grid_image);
+            gridView.setPadding(8, 8, 8, 8);
+            posterImageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+
+            nameTextView.setText(showList.get(position).getName());
+            if (showList.get(position).getImageUrl() != null) {
+                Picasso.with(context).load(showList.get(position).getImageUrl()).into(posterImageView);
+            } else {
+                Picasso.with(context)
+                        .load(R.mipmap.ic_no_image)
+                        .resize(200, 300)
+                        .into(posterImageView);
+            }
         } else {
-            imageView = (ImageView) convertView;
+            gridView = (View) convertView;
         }
 
-        Picasso.with(context).load(showList.get(position).getImageUrl()).into(imageView);
-        return imageView;
+        return gridView;
+
     }
 
 }
